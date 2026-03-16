7a6ce7a067a008704e4b81d276c13bc8cde79098
Cécile Guillot <cecile.guillot@oclock.school>
2026-03-16 12:58:03 +0100

    feat: Hero.tsx

---
+++ client/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+function Hero(){
+    return(
+        <section className="hero">
+            <h1>Relevez les Challenges</h1>
+            <h2>Devenez une légende</h2>
+
+            <p>GamerChallenges : la plateforme ultime pour transformer vos skills en gloire. Challenges exclusifs. Votre voyage vers le sommet commence ici
+            </p>
+            <div className="hero-buttons">
+                <button className="btn-blue">Voir les Challenges</button>
+                <button className="btn-red">Inscription</button>
+            </div>
+        </section>
+    )
+}
+export default Hero
