b4d16bf7050490bf744770bd4a4375c6888e7b42
Cécile Guillot <cecile.guillot@oclock.school>
2026-03-17 14:25:07 +0100

    Feat: navigation pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,21 +4,63 @@ import ChallengeList from "./components/ChallengeList"
 import Leaderboard from "./components/LeaderBord"
 import Sponsors from "./components/Sponsor"
 import Footer from "./components/Footer"
+import { Routes, Route } from "react-router-dom"
 
 
 import "./App.css"
 
 function App() {
-  return(
+  return (
     <>
-    <Header />
-    <Hero />
-    <main className="main-content">
-      <ChallengeList />
-      <Leaderboard />
-    </main>
-    <Sponsors />
-    <Footer />
+      <Header />
+
+      <main className="main-content">
+        <Routes>
+          {/* page d'accueil */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <ChallengeList />
+                <Leaderboard />
+              </>
+            }
+          />
+
+          {/* page liste de challenges */}
+          <Route
+            path="/challenges"
+            element={
+              <>
+                <Hero />
+                <ChallengeList />
+              </>
+            }
+          />
+
+          {/* page leaderboard */}
+          <Route
+            path="/leaderboard"
+            element={<Leaderboard />}
+          />
+
+          {/* page sponsors */}
+          <Route
+            path="/sponsors"
+            element={<Sponsors />}
+          />
+
+          {/* 404 */}
+          <Route
+            path="*"
+            element={<p>Page introuvable</p>}
+          />
+        </Routes>
+      </main>
+
+      <Sponsors />
+      <Footer />
     </>
   )
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,21 +1,27 @@
-import styles from './Header.module.css'
+import { Link } from "react-router-dom"
+import styles from "./Header.module.css"
 
-
-function Header(){
-    return(
-        <header className={styles.header}>
-            <div className={styles.logo}>GamerChallenges</div>
-        <nav>
-            <h2 className={styles.navTitle}>GamerChallenges</h2>
-            <ul className={styles.navlist}>
-                <li className={styles.navItem}>Accueil</li>
-                <li className={styles.navItem}>Liste des Challenges</li>
-                <li className={styles.navItem}>Classement</li>     
-            </ul>
-        </nav>
-        <button className={styles.loginBtn}>Connexion</button>
-        </header>
-    )
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>GamerChallenges</div>
+      <nav>
+        <h2 className={styles.navTitle}>GamerChallenges</h2>
+        <ul className={styles.navlist}>
+          <li className={styles.navItem}>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link to="/challenges">Liste des Challenges</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link to="/leaderboard">Classement</Link>
+          </li>
+        </ul>
+      </nav>
+      <button className={styles.loginBtn}>Connexion</button>
+    </header>
+  )
 }
 
 export default Header
