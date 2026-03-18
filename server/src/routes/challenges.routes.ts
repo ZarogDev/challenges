419d1dc3283f5658b8b812aa70dfe22aabbee74a
vHannoyer <166146207+vHannoyer@users.noreply.github.com>
2026-03-18 15:06:40 +0100

    feat: Add GET /api/challenges/:id/participations route

--- server/src/routes/challenges.routes.ts
+++ server/src/routes/challenges.routes.ts
@@ -2,18 +2,22 @@ import { Router } from "express";
 import {
   getAllChallenges,
   getChallengeById,
-  createChallenge
+  createChallenge,
+  getChallengeParticipations
 } from "../controllers/challenges.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { validateCreationBody } from "../middlewares/challenges.middleware.js";
 
 const router = Router();
 
-// voir tous les challenges
+// récupérer tous les challenges
 router.get("/", getAllChallenges);
 
-// voir un challenge avec son id
+// récupérer un challenge avec son id
 router.get("/:id", getChallengeById);
+
+// récupérer les participations d'un challenge avec son id
+router.get("/:id/participations", getChallengeParticipations);
 
 // créer un challenge
 router.post("/", authenticate, validateCreationBody, createChallenge);
