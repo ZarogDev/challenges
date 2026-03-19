d7ee549f150cd5e77aa37bacff16d11abac906bc
vHannoyer <166146207+vHannoyer@users.noreply.github.com>
2026-03-19 10:55:47 +0100

    feat(client): Update ChallengeList component

--- client/src/components/ChallengeList.tsx
+++ client/src/components/ChallengeList.tsx
@@ -1,56 +1,7 @@
-
 import React, { useState, useEffect } from "react"
 import styles from "./ChallengeList.module.css"
 import ChallengeCard from "./ChallengeCard"
-
-type Challenge = {
-  id: number
-  image: string
-  title: string
-  creator: string
-}
-
-
-
-//const mockChallenges = [
-//  {
-//    id: 1,
-//    image: '/minecraft.jpg',
-//    title: 'Speed Run Minecraft :\nTerminez le mode Survie',
-//    creator: 'Gameone',
-//  },
-//  {
-//    id: 2,
-//    image: '/apex.jpg',
-//    title: 'Dominante Suprême :\nGagnez 3 Matchs de Suite',
-//    creator: 'GamerOne',
-//  },
-//  {
-//    id: 3,
-//    image: '/valorant.jpg',
-//    title: "L'As Tactique :\nÉliminez l'Équipe Adverse",
-//    creator: 'GamerOne',
-//  },
-//  {
-//    id: 4,
-//    image: '/csgo.jpg',
-//    title: 'Le Gardien du Site :\nDésamorcez 3 Bombes',
-//   creator: 'GamerOne',
-//  },
-//  {
-//    id: 5,
-//   image: '/fortnite.jpg',
-//   title: 'Le Dernier Survivant :\nTop 1 sans élimination',
-//   creator: 'GamerOne',
-//  },
-//  {
-//    id: 6,
-//    image: '/rocket.jpg',
-//   title: 'Rocket League :\nMarquez 5 buts en ranked',
-//   creator: 'GamerOne',
-// },
-//];
-
+import type { Challenge } from "../@types"
 
 const ChallengeList: React.FC = () => {
   const [challenges, setChallenges] = useState<Challenge[]>([])
@@ -60,7 +11,7 @@ const ChallengeList: React.FC = () => {
   useEffect(() => {
     const fetchChallenges = async () => {
       try {
-        const res = await fetch("http://localhost:3000/api/challenges")
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/challenges`)
         if (!res.ok) {
           throw new Error("Erreur lors du chargement des challenges")
         }
@@ -110,9 +61,7 @@ const ChallengeList: React.FC = () => {
             {challenges.map((c) => (
               <ChallengeCard
                 key={c.id}
-                image={c.image}
-                title={c.title}
-                creator={c.creator}
+                challenge={c}
               />
             ))}
           </div>
