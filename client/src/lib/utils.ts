c3152652dcce6fb837cd99fa7f66dfb5f809e747
vHannoyer <166146207+vHannoyer@users.noreply.github.com>
2026-03-23 16:17:18 +0100

    feat(client): Add function to get youtube embed link from db link

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -80,3 +80,8 @@ export async function isValidRegister(username: string, email: string, password:
     return { valid: false, messages: [{ global: "Une erreur inconnue est survenue"}] };
   }
 }
+
+export function getEmbedUrl(url: string) {
+  const videoId = url.split("v=")[1];
+  return `https://www.youtube.com/embed/${videoId}`;
+};
