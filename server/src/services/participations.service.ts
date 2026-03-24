6273fd65e60b1e9abd41fd08b3c37a1659aba046
vHannoyer <166146207+vHannoyer@users.noreply.github.com>
2026-03-24 11:20:58 +0100

    feat(server): Add participations.service.ts file with createVoteOnParticipation

---
+++ server/src/services/participations.service.ts
@@ -0,0 +1,25 @@
+import { prisma } from "../db/prisma";
+
+export async function createVoteOnParticipation(participationId: number, userId: number, rating: number) {
+  const existingVoteOnParticipation = await prisma.voteParticipation.findFirst({
+    where: { participationId, userId }
+  });
+
+  if(existingVoteOnParticipation) {
+    return { success: false, error: "A user can only vote once for a participation", status: 400 }
+  }
+  
+  try {
+    const newVote = await prisma.voteParticipation.create({
+      data: { 
+        participationId,
+        userId, 
+        rating
+      }
+    });
+
+    return { success: true, data: newVote, status: 201 };
+  } catch {
+    return { success: false, error: "Internal server error", status: 500 };
+  }
+}
