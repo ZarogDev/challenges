2d79d0c5fe704c2698356a683b9905e4cf2caabb
vHannoyer <166146207+vHannoyer@users.noreply.github.com>
2026-03-24 11:19:27 +0100

    feat(server): Add POST /api/participations/:id/votes route

--- server/src/routes/participations.routes.ts
+++ server/src/routes/participations.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { createParticipation, getChallengeParticipations } from "../controllers/participation.controller";
+import { createParticipation, getChallengeParticipations, voteOnParticipation } from "../controllers/participation.controller";
 import { authenticate } from "../middlewares/auth.middleware";
+import { validateVoteBody } from "../middlewares/common.middleware";
 
 const router = Router();
 
@@ -9,5 +10,8 @@ router.post("/challenges/:id/participations", authenticate, createParticipation)
 
 // voir les participations d'un challenge
 router.get("/challenges/:id/participations", getChallengeParticipations);
+
+// voter pour un challenge
+router.post("/participations/:id/votes", authenticate, validateVoteBody, voteOnParticipation);
 
 export default router;
