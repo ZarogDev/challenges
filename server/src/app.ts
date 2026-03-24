a115ba4c4210e5b98a8f91386f4f61f6dec79778
vHannoyer <166146207+vHannoyer@users.noreply.github.com>
2026-03-24 11:21:23 +0100

    fix(server): Fix import errors in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import routes from "./routes/index.js";
-import { globalErrorHandler } from "./middlewares/error-handler.middleware.js";
+import routes from "./routes/index";
+import { globalErrorHandler } from "./middlewares/error-handler.middleware";
 
 const app = express();
 
