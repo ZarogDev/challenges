ed9a9d479a22eb841bb57e4458025402bd11aa5d
vHannoyer <166146207+vHannoyer@users.noreply.github.com>
2026-03-24 10:52:23 +0100

    feat(server): Update validateBody common middleware to prettify Zod errors

--- server/src/middlewares/common.middleware.ts
+++ server/src/middlewares/common.middleware.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Request, Response } from "express";
-import { ZodObject } from "zod";
+import z, { ZodError, ZodObject } from "zod";
 
 export async function validateBody(schema: ZodObject, req: Request, res: Response, next: NextFunction) {
   try {
     await schema.parseAsync(req.body);
     next();
   } catch (error) {
-    if(!(error instanceof Error)) {
-      return res.status(500).json({ error: 'Erreur de validation inconnue' });
+    if(error instanceof ZodError) {
+      return res.status(400).json({ error: z.prettifyError(error) });
     }
 
-    res.status(400).json({ error: error.message });
+    return res.status(500).json({ error: "Internal server error" });
   }
 }
