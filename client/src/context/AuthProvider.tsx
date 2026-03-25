bf946beb6eedd47e45f7b646208884bac6ec92bb
vHannoyer <166146207+vHannoyer@users.noreply.github.com>
2026-03-25 15:29:57 +0100

    fix(client): Update AuthProvider

--- client/src/context/AuthProvider.tsx
+++ client/src/context/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import { useNavigate } from "react-router-dom";
 
@@ -7,19 +7,39 @@ export default function AuthProvider({
 }: {
   children: React.ReactNode;
 }) {
-  // les states sont initialisés directement avec le contenu du localStorage, pour éviter la déconnexion au rechargement de la page
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
-  const [token, setToken] = useState<string | null>(localStorage.getItem('token'));
-  const [username, setUsername] = useState<string | null>(localStorage.getItem('user'));
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  const localUser = localStorage.getItem('user');
-  const localToken = localStorage.getItem('token');
+  useEffect(() => {
+    async function fetchUser() {
+      const localToken = localStorage.getItem('token');
+
+      if(!localToken) return;
+
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/me`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${localToken}`
+        }
+      });
+
+      // gestion d'erreurs à faire ici
+      if(!response.ok) return;
+
+      const data = await response.json();
+      setUsername(data.username);
+      setToken(localToken);
+      setIsLoggedIn(true);
+    }
+    
+    fetchUser();
+  }, []);
 
   const navigate = useNavigate();
 
   const login = async (email: string, password: string) => {
-    if(!localUser || !localToken) {
       const response = await fetch('http://localhost:3000/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -37,12 +57,10 @@ export default function AuthProvider({
         localStorage.setItem('token', data.token);
       } else {
         setError('Erreur de connexion ! Vérifiez vos identifiants.');
+        setIsLoggedIn(false);
+        setToken(null);
+        setUsername(null);
       }
-    } else {
-      setUsername(localUser);
-      setToken(localToken);
-      setIsLoggedIn(true);
-    }
   };
 
   const logout = () => {
